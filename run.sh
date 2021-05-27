9d5a25ee75a0d1d5deedc4cbe0a84e2036a50733
Tamas Pasztor <tomcsi@tomcsi.hu>
2021-05-27 13:43:28 +0200

    Add github workflow

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 set -e
 
 echo "$AWS_KEY:$AWS_SECRET_KEY" > passwd && chmod 600 passwd
-s3fs "$S3_BUCKET" "$MNT_POINT" -o passwd_file=passwd  && tail -f /dev/null
+s3fs $S3_BUCKET $MNT_POINT -o passwd_file=passwd -o use_path_request_style -o endpoint=hu2-k3s -o url=$S3_URL && tail -f /dev/null
